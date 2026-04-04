377c7254cd116b4a3f847f5db0e32faadd27647d
jfsosag <jaunfesosa10@gmail.com>
2026-04-03 22:34:00 -0400

    123

--- src/VISUAL/Menu.java
+++ src/VISUAL/Menu.java
@@ -212,7 +212,7 @@ public class Menu extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void menu_mant_usuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menu_mant_usuActionPerformed
-    FrmUsuario01 frm = new FrmUsuario01();
+    FrmUsuario frm = new FrmUsuario();
     frm.setVisible(true);
     }//GEN-LAST:event_menu_mant_usuActionPerformed
 
